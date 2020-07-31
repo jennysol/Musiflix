712a1a7513326ff670c0ebb3ebb4e580f95c7b99
jennysol <jennyssoliver@gmail.com>
2020-07-30 22:08:05 -0300

    Integração com o Servidor Heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     console.log('alo alo alo');
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = 'https://soliver.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
