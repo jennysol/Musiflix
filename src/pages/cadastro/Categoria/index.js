7d46215109af159d9ee4685221f28c07c1275586
jennysol <jennyssoliver@gmail.com>
2020-08-01 12:04:08 -0300

    Finalização 0.1

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -19,7 +19,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://soliver.herokuapp.com/categorias';
+      : 'https://devsoutinhoflix.herokuapp.com/categorias';
     // E a ju ama variáveis
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
